a36a6cc66c66d39edf002725b75ece3b9d964134
junyang <yang.jun3@husky.neu.edu>
2020-05-14 16:45:31 -0400

    add clear button

--- markdown.js
+++ markdown.js
@@ -5,9 +5,7 @@ window.onload=function(){
     var changeEditTextToMarkdownText=function(){
         var markdownText=editArea.value;
         var html = converter.makeHtml(markdownText);
-        editedArea.innerHTML=html;
-        // console.log(group);
-        
+        editedArea.innerHTML=html;      
     }
     
     editArea.addEventListener("input",changeEditTextToMarkdownText);
@@ -20,9 +18,6 @@ const textColor=getComputedStyle(document.body).getPropertyValue('--text-color')
 const fontSelect = document.getElementById('fontPicker');
 const bgColorSelect=document.getElementById('bgColorPicker');
 const textColorSelect = document.getElementById('textColorPicker');
-// const fontSelect = document.querySelector('.fontPicker');
-// const bgColorSelect=document.querySelector('.bgColorPicker');
-// const textColorSelect = document.querySelector('.textColorPicker');
 
 const group=[fontSelect,bgColorSelect,textColorSelect];
 group.forEach(function(option){
@@ -30,3 +25,10 @@ group.forEach(function(option){
         document.body.style.setProperty(option.dataset.themeValue,event.target.value);
     })
 })
+
+function clearTextArea(){
+    var textArea = document.getElementById('inputText');
+    var editedArea=this.document.getElementById("resultText");
+    editedArea.innerHTML=""
+    textArea.value="";
+}
