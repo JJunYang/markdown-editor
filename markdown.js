d6b0ffd49d07f79893a1c9f5760505ac97d07cd2
junyang <yang.jun3@husky.neu.edu>
2020-05-14 16:38:22 -0400

    add theme selector

--- markdown.js
+++ markdown.js
@@ -6,8 +6,27 @@ window.onload=function(){
         var markdownText=editArea.value;
         var html = converter.makeHtml(markdownText);
         editedArea.innerHTML=html;
+        // console.log(group);
+        
     }
-
+    
     editArea.addEventListener("input",changeEditTextToMarkdownText);
     changeEditTextToMarkdownText();
 }
+
+const bodyFont = getComputedStyle(document.body).getPropertyValue('--font');
+const bgColor = getComputedStyle(document.body).getPropertyValue('--bg-color');
+const textColor=getComputedStyle(document.body).getPropertyValue('--text-color');
+const fontSelect = document.getElementById('fontPicker');
+const bgColorSelect=document.getElementById('bgColorPicker');
+const textColorSelect = document.getElementById('textColorPicker');
+// const fontSelect = document.querySelector('.fontPicker');
+// const bgColorSelect=document.querySelector('.bgColorPicker');
+// const textColorSelect = document.querySelector('.textColorPicker');
+
+const group=[fontSelect,bgColorSelect,textColorSelect];
+group.forEach(function(option){
+    option.addEventListener('change',(event)=>{
+        document.body.style.setProperty(option.dataset.themeValue,event.target.value);
+    })
+})
